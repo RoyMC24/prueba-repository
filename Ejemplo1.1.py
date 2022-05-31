c9adae91c1e1136f8e767ac7797380d018ae5032
RoyMC24 <rodrigo.mayac@alumno.buap.mx>
2022-05-30 23:16:23 -0500

    initial commit

---
+++ Ejemplo1.1.py
@@ -0,0 +1,12 @@
+calificacionJOSE = input("Introduce tu nota de E.D: ")
+
+calificacionJOSE = int(calificacionJOSE)
+
+if calificacionJOSE < 700 :
+    print("No mamar. ")
+elif calificacionJOSE > 1000 :
+    print("PINCHE MENTIROSO ")
+else :
+    print("NADA. ") 
+    
+
